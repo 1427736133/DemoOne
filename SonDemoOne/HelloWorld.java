c0666268e5c7c3e36f3bdce9d1439adba13984fc
jiang_tao1996 <1427736133@qq.com>
2020-09-05 22:48:17 +0800

    delete MainController

---
+++ SonDemoOne/HelloWorld.java
@@ -0,0 +1,210 @@
+
+|  .+..= o        |
+|    =o .         |
++----[SHA256]-----+
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ pwd
+/c/Users/admin/.ssh
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ LS
+bash: caLS: command not found
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ ls
+id_rsa  id_rsa.pub  known_hosts
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ car id_rsa.pub
+bash: car: command not found
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ cat id_rsa.pub
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLRrI5c20FeA5UOTinFMWLMIa2G/7t7+/QdEziI8YqNTSF         NDN/au/BOxk9FulQW5P2IUsVOS6qFX14tem3TaYjgVtXlQy8i1H/ADgITC4+O5fWuh/cvP0t4DsoSP2eeGcU         w/uJcXOZfUIYMw0Cyi7rqV8dOnAVFOLXMMyqXNtXNTHPwPLl4gDjSVPVuQKNofzSD9y0KucwoMfZ3wIBqpJS         DFi+coGF3q6lWDMak/IvjvXF2m6k108mOo14hNhxxvN8hWPHOlrO8rymmVXFd6i9kLo/LC/jmBKYd3NFJgyA         lnp0Hm4JmBzDCX2spnblqR5Wq2hHftXbGqmId7RTJcrJ 1427736133@qq.com
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ ^C
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ cat ~/.ssh/id_rsa.pub
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLRrI5c20FeA5UOTinFMWLMIa2G/7t7+/QdEziI8YqNTSFNDN/au/BOxk9FulQW5P2IUsVOS6qFX14tem3TaYjgVtXlQy8i1H/ADgITC4+O5fWuh/cvP0t4DsoSP2eeGcUw/uJcXOZfUIYMw0Cyi7rqV8dOnAVFOLXMMyqXNtXNTHPwPLl4gDjSVPVuQKNofzSD9y0KucwoMfZ3wIBqpJSDFi+coGF3q6lWDMak/IvjvXF2m6k108mOo14hNhxxvN8hWPHOlrO8rymmVXFd6i9kLo/LC/jmBKYd3NFJgyAlnp0Hm4JmBzDCX2spnblqR5Wq2hHftXbGqmId7RTJcrJ 1427736133@qq.com
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ pwd
+/c/Users/admin/.ssh
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ clear
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ pwd
+/c/Users/admin/.ssh
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ pwd
+/c/Users/admin/.ssh
+
+admin@DESKTOP-TDV5J49 MINGW64 ~/.ssh
+$ cd G:
+
+admin@DESKTOP-TDV5J49 MINGW64 /g
+$ pwd
+/g
+
+admin@DESKTOP-TDV5J49 MINGW64 /g
+$ cd GitRepostory/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ pwd
+/g/GitRepostory
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git clone git@github.com:1427736133/DemoOne.git
+Cloning into 'DemoOne'...
+warning: You appear to have cloned an empty repository.
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ ls
+DemoOne/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ cd DemoOne/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/DemoOne (master)
+$ ls -a
+./  ../  .git/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/DemoOne (master)
+$ cd ..
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ pwd
+/g/GitRepostory
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ ls
+DemoOne/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ mkdir TestDemo
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ ls
+DemoOne/  TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ cd TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/TestDemo (master)
+$ touch ClassTestDemo.java
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/TestDemo (master)
+$ ls
+ClassTestDemo.java
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/TestDemo (master)
+$ vi ClassTestDemo.java
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/TestDemo (master)
+$ vim ClassTestDemo.java
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/TestDemo (master)
+$ pwd
+/g/GitRepostory/TestDemo
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory/TestDemo (master)
+$ cd ..
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ pwd
+/g/GitRepostory
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ ls
+DemoOne/  TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git status
+On branch master
+
+Initial commit
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+        TestDemo/
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ ls
+DemoOne/  TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git add TestDemo/
+warning: LF will be replaced by CRLF in TestDemo/ClassTestDemo.java.
+The file will have its original line endings in your working directory.
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git status
+On branch master
+
+Initial commit
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+
+        new file:   TestDemo/ClassTestDemo.java
+
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git commit -m "first commit.."
+[master (root-commit) 5749992] first commit..
+ 1 file changed, 8 insertions(+)
+ create mode 100644 TestDemo/ClassTestDemo.java
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git status
+On branch master
+nothing to commit, working tree clean
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git push -u origin master
+fatal: 'origin' does not appear to be a git repository
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git status
+On branch master
+nothing to commit, working tree clean
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git commit -m"first commit.."
+On branch master
+nothing to commit, working tree clean
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ pwd
+/g/GitRepostory
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ ls
+DemoOne/  TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git add TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git status
+On branch master
+nothing to commit, working tree clean
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$ git add TestDemo/
+
+admin@DESKTOP-TDV5J49 MINGW64 /g/GitRepostory (master)
+$
